b1f45ca2ea822a76c020286517cd7652c7468106
Stefano Straga <stefanostraga2k18@gmail.com>
2023-04-18 16:20:13 +0200

    add ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:chatgptcurso/home.dart';
 import 'package:chatgptcurso/home.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
@@ -12,12 +15,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage());
   }
 }
-
