a3af84eecc22d1150865bb017d239e4bb0e45b91
Stefano Straga <stefanostraga2k18@gmail.com>
2023-04-18 03:57:11 +0200

    gitignoree

--- lib/services/open_ai_services.dart
+++ lib/services/open_ai_services.dart
@@ -5,25 +5,24 @@ String apiKey = "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";
 
 Future sendChatCompletionRequest(String message) async {
   String baseUrl = "https://api.openai.com/v1/chat/completions";
-  Map<String,String> headers = {
+  Map<String, String> headers = {
     "Content-Type": "application/json; charset=UTF-8",
     "Authorization": "Bearer $apiKey"
   };
 
-  var res = await http.post(
-    Uri.parse(baseUrl),
-    headers: headers,
-    body: json.encode({
-      "model": "gpt-3.5-turbo",
-      "messages": [
-        {"role":"user", "content": message}
-      ]
-    })
-  );
+  var res = await http.post(Uri.parse(baseUrl),
+      headers: headers,
+      body: json.encode({
+        "model": "gpt-3.5-turbo",
+        "messages": [
+          {"role": "user", "content": message}
+        ]
+      }));
 
-  if(res.statusCode == 200){
+  if (res.statusCode == 200) {
     final resultBody = utf8.decode(res.bodyBytes);
-    final String result = jsonDecode(resultBody)["choices"][0]["message"]["content"];
+    final String result =
+        jsonDecode(resultBody)["choices"][0]["message"]["content"];
     return result.trim();
   }
 }
